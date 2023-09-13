bc267c3dee35555f746369a582344df825173501
Adriano <adriano.ungarelli@gmail.com>
2023-09-13 18:24:54 -0300

    Add CT 'search with sucess'

--- cypress/e2e/blogdoagi.cy.js
+++ cypress/e2e/blogdoagi.cy.js
@@ -1,9 +1,13 @@
 describe('search', () => {
   beforeEach(() => {
     cy.visit('https://blogdoagi.com.br')
+    cy.viewport(1920, 1080)
   })
-  it('search with sucess', () => {
-    
+  it.only('search with sucess', () => {
+    cy.get('#search-open').click()
+    cy.get('.desktop-search > .search-form > label > .search-field').type('Serviços')
+    cy.get('input.search-submit:first').click();
+    cy.get('.archive-title').should('include.text', 'Resultados da busca por: Serviços');
 
   })
   it('search with sucess - long text', () => {
