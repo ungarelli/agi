67e834b77e44a8e4121484a8848cc719d3d82532
Adriano <adriano.ungarelli@gmail.com>
2023-09-13 18:31:24 -0300

    Add CT 2 and 3

--- cypress/e2e/blogdoagi.cy.js
+++ cypress/e2e/blogdoagi.cy.js
@@ -3,7 +3,7 @@ describe('search', () => {
     cy.visit('https://blogdoagi.com.br')
     cy.viewport(1920, 1080)
   })
-  it.only('search with sucess', () => {
+  it('search with sucess', () => {
     cy.get('#search-open').click()
     cy.get('.desktop-search > .search-form > label > .search-field').type('Serviços')
     cy.get('input.search-submit:first').click();
@@ -11,11 +11,18 @@ describe('search', () => {
 
   })
   it('search with sucess - long text', () => {
-    
+    cy.get('#search-open').click()
+    cy.get('.desktop-search > .search-form > label > .search-field').type('long text - long text - long text - long text - long text - long text - long text - long text - long text')
+    cy.get('input.search-submit:first').click();
+    //entry-title element
+    cy.get('.entry-title').should('include.text', 'Nenhum resultado');
     
   })
   it('search - results not found', () => {
-    
+    cy.get('#search-open').click()
+    cy.get('.desktop-search > .search-form > label > .search-field').type('Not found')
+    cy.get('input.search-submit:first').click();
+    cy.get('.archive-title').should('include.text', 'Resultados da busca por: Not found');
     
   })
 })
